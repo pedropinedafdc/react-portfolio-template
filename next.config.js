710de8f091adb3c754acdb2795b39cde71fb6bc9
pedropinedafdc <39924969+pedropinedafdc@users.noreply.github.com>
2025-03-11 16:23:39 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.externals = {
+      "custom-cursor-react": "commonjs custom-cursor-react",
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
